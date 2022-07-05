79377d7fe8eacb4373c757a3182be47061f74645
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-07-05 22:58:16 +0000

    update script.R docs

--- inst/script.R
+++ inst/script.R
@@ -1,2 +1,18 @@
-# the script should be written here to benchmark
-print("It is running the script")
+# The script should be written here to benchmark the package.
+# NOTE: save_data argument must be set to TRUE for plot_metrics functions.
+
+## TEST 1
+# Rperform::plot_metrics(
+#     test_path = "inst/tests/test-join.r",
+#     metric = "time", num_commits = 5, save_data = TRUE,
+#     save_plots = TRUE,
+# )
+
+
+## TEST 2
+# Rperform::plot_metrics(
+#     test_path = "inst/tests/test-join.r",
+#     metric = "memory", num_commits = 5, save_data = TRUE,
+#     save_plots = TRUE,
+# )
+
