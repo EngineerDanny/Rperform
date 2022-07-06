491f895e5c60bbb4543990e213316c8b0f4992f9
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-07-06 00:37:56 +0000

    update script docs

--- inst/script.R
+++ inst/script.R
@@ -1,4 +1,4 @@
-# The script should be written here to benchmark the package.
+# The script should be written here to benchmark the package on Github.
 # NOTE: save_data argument must be set to TRUE for plot_metrics functions.
 
 ## TEST 1
@@ -16,3 +16,14 @@
 #     save_plots = TRUE,
 # )
 
+## TEST 3
+# Rperform::time_compare(
+#    test_path = "inst/tests/test-dup.r",
+#    num_commits = 2, save_data = TRUE,
+# )
+
+## TEST 4
+# Rperform::mem_compare(
+#    test_path = "inst/tests/test-dup.r",
+#    num_commits = 2, save_data = TRUE,
+# )
